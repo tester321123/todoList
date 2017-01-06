10a1b88d534da32d17bb527d74376fcd7359c9c6
tester321123 <testerrt@yahoo.de>
2017-01-06 22:26:49 +0100

    from git blabla

--- api.js
+++ api.js
@@ -1,3 +1,3 @@
 
-did add test text in order to test git  ......my additional text......MY NEW TEXT TEXT FROM GIT OR OTHER DEVELOPER
+did add test text in order to test git  ......my additional text......MY NEW TEXT TEXT FROM GIT OR OTHER DEVELOPER blablablfromgit
 
